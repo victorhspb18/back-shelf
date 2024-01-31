0a9086685e1d8a520effcf1871778cda1c1746b0
victorhspb18 <victorhspb18@gmail.com>
2024-01-31 20:57:03 -0300

    Criação do projeto

---
+++ bin/back_shelf.dart
@@ -0,0 +1,5 @@
+import 'package:back_shelf/back_shelf.dart' as back_shelf;
+
+void main(List<String> arguments) {
+  print('Hello world: ${back_shelf.calculate()}!');
+}
---
+++ test/back_shelf_test.dart
@@ -0,0 +1,8 @@
+import 'package:back_shelf/back_shelf.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

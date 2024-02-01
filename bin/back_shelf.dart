5760e8943a17c4716ef6c2d63eafb59ab9ec10f9
victorhspb18 <victorhspb18@gmail.com>
2024-01-31 21:20:13 -0300

    Criando um servidor

--- bin/back_shelf.dart
+++ bin/back_shelf.dart
@@ -1,5 +1,12 @@
-import 'package:back_shelf/back_shelf.dart' as back_shelf;
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as server_io;
 
-void main(List<String> arguments) {
-  print('Hello world: ${back_shelf.calculate()}!');
+void main() async {
+  final server = await server_io.serve(
+    (request) => Response(200, body: 'ok'),
+    'localhost',
+    8080,
+  );
+
+  print('Nosso servido foi iniciado http://localhost:8080');
 }

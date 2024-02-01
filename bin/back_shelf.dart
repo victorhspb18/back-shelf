b51a7630e8be10933730ce565fdf15702c2b181e
Victor Hugo <victor.hugo@external.fcagroup.com>
2024-02-01 20:47:56 -0300

    Configuracao no vscode

--- bin/back_shelf.dart
+++ bin/back_shelf.dart
@@ -3,10 +3,15 @@ import 'package:shelf/shelf_io.dart' as server_io;
 
 void main() async {
   final server = await server_io.serve(
-    (request) => Response(200, body: 'ok'),
+    (request) {
+      if (request.url.path.contains('80')) {
+        return Response(200, body: 'ok');
+      }
+      return Response(404, body: 'Nada encontrado');
+    },
     'localhost',
     8080,
   );
 
-  print('Nosso servido foi iniciado http://localhost:8080');
+  print('Nosso servidor foi iniciado em ${server.port}');
 }
--- test/back_shelf_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:back_shelf/back_shelf.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
